d442757a66d53e9afdc214bc9d3fa7aa6a2f3538
Fish Walker <havanlinh19042000@gmail.com>
2023-07-11 09:03:28 +0700

    Show image

--- SportsSln/SportsStore/Models/SeedData.cs
+++ SportsSln/SportsStore/Models/SeedData.cs
@@ -22,15 +22,15 @@ namespace SportsStore.Models
                     Description = "A boat for one person",
                     Category = "Watersports",
                     Price = 275,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-001.jpg"
                 },
                 new Product
                 {
                     Name = "Lifejacket",
-                    Description = "Protective and fashionable",
+                    Description = "Protective and fashionagfl",
                     Category = "Watersports",
                     Price = 48.95m,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-002.jpg"
                 },
                 new Product
                 {
@@ -38,7 +38,7 @@ namespace SportsStore.Models
                     Description = "FIFA-approved size and weight",
                     Category = "Soccer",
                     Price = 19.50m,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-003.jpg"
                 },
                 new Product
                 {
@@ -46,7 +46,7 @@ namespace SportsStore.Models
                     Description = "Give your playing field a professional touch",
                     Category = "Soccer",
                     Price = 34.95m,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-004.jpg"
                 },
                 new Product
                 {
@@ -54,7 +54,7 @@ namespace SportsStore.Models
                     Description = "Flat-packed 35,000-seat stadium",
                     Category = "Soccer",
                     Price = 79500,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-005.jpg"
                 },
                 new Product
                 {
@@ -62,7 +62,7 @@ namespace SportsStore.Models
                     Description = "Improve brain efficiency by 75%",
                     Category = "Chess",
                     Price = 16,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-006.jpg"
                 },
                 new Product
                 {
@@ -70,7 +70,7 @@ namespace SportsStore.Models
                     Description = "Secretly give your opponent a disadvantage",
                     Category = "Chess",
                     Price = 29.95m,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-007.jpg"
                 },
                 new Product
                 {
@@ -78,7 +78,7 @@ namespace SportsStore.Models
                     Description = "A fun game for the family",
                     Category = "Chess",
                     Price = 75,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-008.jpg"
                 },
                 new Product
                 {
@@ -86,7 +86,7 @@ namespace SportsStore.Models
                     Description = "Gold-plated, diamond-studded King",
                     Category = "Chess",
                     Price = 1200,
-                    Image = "/images/project-17.jpg"
+                    Image = "/images/hoalan-009.jpg"
                 }
                 );
                 context.SaveChanges();
